60dde56fed103ca8e17d73513b267a9f7111fb5d
helldemid <hellboy08090@gmail.com>
2024-02-11 18:20:57 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import mainGif from '../src/media/cute.gif';
+import noGif from '../src/media/fight.gif';
+import kiss from '../src/media/kiss.gif';
+
+const titles = {
+  main: "Ти будеш моєю Валентинкою?",
+  no: "Альо, ти що там задумала?",
+  yes: "Ти моя бусінка🥰"
+}
+
+function getRandomPosition() {
+  const maxX = window.innerWidth - 50;
+  const maxY = window.innerHeight - 50;
+
+  return { left: Math.floor(Math.random() * (maxX + 1)), top: Math.floor(Math.random() * (maxY + 1)) };
+}
+
+function Buttons({ onNoClick, onYesClick, onNoEntered, isVisible }) {
+  return (
+    <div className='main-buttons' style={{ display: isVisible ? 'flex' : 'none' }}>
+      <button id='no' onClick={onNoClick} onMouseEnter={onNoEntered}>Ні</button>
+      <button id='yes' onClick={onYesClick}>Так!</button>
+    </div>
+  )
+}
 
 function App() {
+
+  const [currentTitle, setCurrentTitle] = useState(titles.main);
+  const [currentGif, setCurrentGif] = useState(mainGif);
+  const [isVisible, setVisibility] = useState(true);
+  const [isVisibleButtons, setVisibilityOfButtons] = useState(true);
+  const [isNoButtonClicked, setIsNoButtonClicked] = useState(false);
+
+  const moveButton = () => {
+    const elem = document.getElementById("no");
+    elem.style.position = 'absolute';
+    elem.style.left = `${getRandomPosition().left}px`;
+    elem.style.top = `${getRandomPosition().top}px`;
+  }
+
+  const handleNoClick = () => {
+    setCurrentTitle(titles.no);
+    setCurrentGif(noGif);
+    setVisibility(false);
+    if(!isNoButtonClicked) {
+      setIsNoButtonClicked(true);
+      moveButton()
+    } else {
+      moveButton()
+    }
+  }
+
+  const handleYesClick = () => {
+    setCurrentTitle(titles.yes);
+    setCurrentGif(kiss);
+    setVisibility(false);
+    setVisibilityOfButtons(false);
+  }
+
+  const handleNoEntered = () => {
+    if (isNoButtonClicked) {
+      moveButton();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="main">
+      <img className="main-gif" src={currentGif} alt="Щось пішло не так(" />
+      <h1 style={{ display: isVisible ? 'block' : 'none' }}>Привіт, красуня!</h1>
+      <p>{currentTitle}</p>
+      <Buttons onNoClick={handleNoClick} onYesClick={handleYesClick} onNoEntered={handleNoEntered} isVisible={isVisibleButtons}></Buttons>
+    </section>
   );
 }
 
